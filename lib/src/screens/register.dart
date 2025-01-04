3f14191656891b965f8ccf33b52b95734cfd420b
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2025-01-04 13:46:34 -0300

    criação do rating com estrelas e começo do sqlite

--- lib/src/screens/register.dart
+++ lib/src/screens/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class Register extends StatefulWidget {
   const Register({super.key});
@@ -10,6 +11,8 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   bool? isReading;
   TextEditingController dateController = TextEditingController();
+  int rating = 0;
+  String comment = "";
 
   Future<void> _showDatePicker() async {
     DateTime? _pickedDate = await showDatePicker(
@@ -97,6 +100,48 @@ class _RegisterState extends State<Register> {
                       ),
                       readOnly: true,
                     )),
+                    Text("Quantas estrelas daria para a obra?", style: TextStyle(fontSize: 20)),
+                    const SizedBox(height: 10),
+                    RatingBar.builder(
+                      initialRating: 0,
+                      minRating: 0,
+                      direction: Axis.horizontal,
+                      allowHalfRating: true,
+                      itemCount: 5,
+                      itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                      itemBuilder: (context, _) => const Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      ),
+                      onRatingUpdate: (rating) {
+                        setState(() {
+                          this.rating = rating.toInt();
+                        });
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                    Text("Gostaria de deixar alguma anotação?", style: TextStyle(fontSize: 20)),
+                    const SizedBox(height: 10),
+                    Padding(padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
+                    child:TextField(
+                      onChanged: (value) {
+                        setState(() {
+                          comment = value;
+                        });
+                      },
+                      decoration: InputDecoration(
+                        labelText: "Escreva aqui",
+                        filled: true,
+                        prefixIcon: Icon(Icons.note_alt_outlined),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color.fromRGBO(189, 213, 234, 1)),
+                        ),
+                      ),
+                      maxLines: null,
+                    ))
                   ]
               )
               ),
