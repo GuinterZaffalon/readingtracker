b7e3f9b1db2267a5a39db01aee24c36bdb9c4c4f
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2025-01-04 12:22:55 -0300

    feat: picker de data no cqdastro de já lido

--- lib/src/screens/register.dart
+++ lib/src/screens/register.dart
@@ -8,28 +8,103 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  bool? isReading;
+  TextEditingController dateController = TextEditingController();
+
+  Future<void> _showDatePicker() async {
+    DateTime? _pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+
+    if(_pickedDate != null) {
+      setState(() {
+        dateController.text = "${_pickedDate.day}/${_pickedDate.month}/${_pickedDate.year}";
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromRGBO(189, 213, 234, 1),
-          title: const Text("ReadingTracker"),
-          centerTitle: true,
-        ),
-        body: Column(
-          children: [
-            const SizedBox(height: 10),
-            const Text("Acabou a leitura?", style: TextStyle(fontSize: 20)),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(onPressed: () {}, child: Text("Sim"), style: ButtonStyle(padding: MaterialStateProperty.all(EdgeInsets.fromLTRB(40, 10, 40, 10)))),
-                ElevatedButton(onPressed: () {}, child: Text("Não"), style: ButtonStyle(padding: MaterialStateProperty.all(EdgeInsets.fromLTRB(40, 10, 40, 10)))),
-              ],
-            )
-          ],
-        ),
+      appBar: AppBar(
+        backgroundColor: const Color.fromRGBO(189, 213, 234, 1),
+        title: const Text("ReadingTracker"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 10),
+          const Text("Acabou a leitura?", style: TextStyle(fontSize: 20)),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    isReading = false;
+                  });
+                },
+                child: const Text("Sim"),
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all(
+                      const EdgeInsets.fromLTRB(40, 10, 40, 10)),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    isReading = true;
+                  });
+                },
+                child: const Text("Não"),
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all(
+                      const EdgeInsets.fromLTRB(40, 10, 40, 10)),
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Container(
+              child: isReading == null
+                  ? const Text("Escolha uma opção.")
+                  : (isReading!
+                  ? const Text("Ainda estou lendo")
+                  : Column(
+                  children: [
+                    const SizedBox(height: 10),
+                    Text("Quando terminou a leitura?", style: TextStyle(fontSize: 20)),
+                    Padding(padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
+                    child: TextField(
+                      controller: dateController,
+                      onTap: () {
+                        _showDatePicker();
+                      },
+                      decoration: InputDecoration(
+                        labelText: "Insira a data",
+                        filled: true,
+                        prefixIcon: Icon(Icons.calendar_today),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color.fromRGBO(189, 213, 234, 1)),
+                        ),
+                      ),
+                      readOnly: true,
+                    )),
+                  ]
+              )
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
+
 }
