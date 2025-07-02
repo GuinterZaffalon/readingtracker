baa9d9e3d0b24c08a8d9eab5a9628303949f32cb
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2025-07-02 20:29:09 -0300

    dotenv e api do gemini para indicar livros

---
+++ lib/src/model/GeminiAPI.dart
@@ -0,0 +1,46 @@
+import "dart:convert";
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+
+class GeminiAPI {
+  static Future<http.Response> getSimilarBooks(String isbn) async {
+    final apiKey = dotenv.env['GEMINI_API'];
+    final prompt = "Pesquise o livro correspondente a $isbn e em um tom de bate papo, me retorne uma indicação de 5 livros parecidos com ele, com um tom de recomendação!";
+    final url = Uri.parse("https://generativelanguage.googleapis.com/v1beta/models/gemma-3n-e4b-it:generateContent?key=$apiKey");
+    final body = jsonEncode({
+    "contents": [
+      {
+        "parts": [
+          {
+            "text": prompt,
+          },
+        ],
+      },
+    ],
+  });
+    try{
+      final response = await http.post(
+        url,
+        headers: {
+        'Content-Type': 'application/json',
+      },
+      body: body,
+      );
+
+      if (response.statusCode >= 200 && response.statusCode < 300) {
+      // Sucesso
+      return response;
+    } else {
+      // Erro da API
+      return http.Response(response.body, response.statusCode);
+    }
+  } catch (e) {
+    // Erro interno do servidor
+    return http.Response(
+      jsonEncode({'error': 'Internal server error'}),
+      500,
+      headers: {'Content-Type': 'application/json'},
+    );
+  }
+  }
+}
